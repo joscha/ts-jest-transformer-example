b50e020caa707a776ef9b383afee8b56d8949395
Joscha Feth <joscha@feth.com>
2017-03-14 18:26:29 +1100

    make it work with __TS_CONFIG__

--- tools/fileTransformer.js
+++ tools/fileTransformer.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const { transpileIfTypescript } = require('ts-jest/dist/transpile-if-ts');
+const { getJestConfig } = require('ts-jest/dist/utils');
+
+// pass through custom Jest TS config to ts-jest
+global.__TS_CONFIG__ = getJestConfig(path.join(__dirname, '../')).config.globals.__TS_CONFIG__;
 
 module.exports = {
   process(src, filename, config, options) {
     const source = 'export default ' + JSON.stringify(path.basename(filename)) + ';';
     return transpileIfTypescript(`${filename}.ts`, source);
   },
-
-  getCacheKey() {
-    return Math.random()+'';
-  }
 };
